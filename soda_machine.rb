04b0a7a82fd725b3ad9deb3eb55aa871a0c4b4d5
Shawn Tuttle <email": null>
2016-06-20 15:18:38 -0700

    Soda machine goodness

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,23 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.length
   end
 
   def find_soda(soda_brand)
+    if
+      sodas.include?(soda_brand)
+
+    else
+      nil
+    end
+
   end
 
   def sell(soda_brand)
+    #soda_brand
+      @cash = cash + self.price
+    end
   end
 
-end
+
